3ab188f3b337ee29a22dc00a6bccb89f5ec5620b
Javier Carrero <139291609+JavierCarrero@users.noreply.github.com>
2023-07-13 15:45:29 -0400

    Add files via upload

---
+++ trabajos/api-rickandmorty/index.js
@@ -0,0 +1,35 @@
+function getCharacters(done) {
+    
+    const result = fetch("https://rickandmortyapi.com/api/character");
+
+result.then(response => response.json())
+.then(data =>{
+    done(data)
+} );
+
+}
+
+getCharacters(data => {
+    data.results.forEach(personaje => {
+        const article = document.createRange().createContextualFragment(/*html*/`
+        
+        <article>
+
+        <div class="imagen-container"> 
+            <img src="${personaje.image}" alt="Personaje">
+        </div>
+        
+        <h2>${personaje.name}</h2>
+        <span>${personaje.status}</span>
+        
+        </article>
+
+        `
+        );
+
+const main = document.querySelector("main");
+
+main.append(article);
+
+    })
+});
